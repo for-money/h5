27a281cb147fb24f0469d56d466cb7bfec8df39c
师红丹 <wb_hongdan>
2019-09-26 20:26:51 +0800

    '上传物品/交换物品'

---
+++ static/js/exchange-upload-goods.js
@@ -0,0 +1,25 @@
+$(function(){
+    let tabs = $('.tab>li');
+    tabs.on('click',function(){
+        let index = $(this).index();
+        tabs.removeClass('active').eq(index).addClass('active');
+        $('.tabChange').css('display','none').eq(index).css('display','block');
+    });
+    tabs.eq(0).triggerHandler('click');
+
+
+    let selectedColl = $('.exchange-goods>.exchange-goods-info');
+    selectedColl.on('click',function(){
+        let index = $(this).index();
+        selectedColl.removeClass('active').eq(index).addClass('active');
+    });
+    selectedColl.eq(0).triggerHandler('click');
+
+    let isSelected = $('.upload-goods-selected>.upload-goods-thumb');
+    isSelected.on('click',function(){
+        let index = $(this).index();
+        isSelected.removeClass('active').eq(index).addClass('active');
+    });
+    isSelected.eq(0).triggerHandler('click');
+
+})
